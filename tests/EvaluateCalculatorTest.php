fc7c5019d72e8427b66d405902c83f22dc2eed5f
Miguel Valles <byvallees17@gmail.com>
2026-09-17 12:44:48 +0200

    [R] - Add tests to evaluate subtract operations

--- tests/EvaluateCalculatorTest.php
+++ tests/EvaluateCalculatorTest.php
@@ -35,6 +35,22 @@ final class EvaluateCalculatorTest extends TestCase
         $this->assertSame("3", $this->calculator->evaluate("1+2"));
     }
 
+    #[Test]
+    public function evaluateReturnsResultOnAdditionWithMultipleNumbers(): void
+    {
+        $this->assertSame("6", $this->calculator->evaluate("1+2+3"));
+    }
 
+    #[Test]
+    public function evaluateReturnsResultOnSubtraction(): void
+    {
+        $this->assertSame("1", $this->calculator->evaluate("3-2"));
+    }
+
+    #[Test]
+    public function evaluateReturnsResultOnSubtractionWithMultipleNumbers(): void
+    {
+        $this->assertSame("0", $this->calculator->evaluate("3-2-1"));
+    }
 }
 
