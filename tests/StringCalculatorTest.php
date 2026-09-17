3f4a61b7f003c8fd100e030b10145cc48d92e0d0
Miguel Valles <byvallees17@gmail.com>
2026-09-17 08:43:22 +0200

    [R] - Add test for multiplying empty string

--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -18,71 +18,77 @@ final class StringCalculatorTest extends TestCase
     }
 
     #[Test]
-    public function itReturnsZeroForEmptyString(): void
+    public function addReturnsZeroForEmptyString(): void
     {
         $this->assertSame('0', $this->calculator->add(''));
     }
 
     #[Test]
-    public function itReturnsSameNumberForSingleNumbers(): void
+    public function addReturnsSameNumberForSingleNumbers(): void
     {
         $this->assertSame("1", $this->calculator->add("1"));
     }
 
     #[Test]
-    public function itReturnsSumForNumbersSeparatedByCommas(): void
+    public function addReturnsSumForNumbersSeparatedByCommas(): void
     {
         $this->assertSame('3', $this->calculator->add("1,2"));
     }
 
     #[Test]
-    public function itReturnsSumForFloatNumbersSeparatedByCommas(): void
+    public function addReturnsSumForFloatNumbersSeparatedByCommas(): void
     {
         $this->assertSame('3.3', $this->calculator->add("1.1,2.2"));
     }
 
     #[Test]
-    public function itReturnsSumForFloatNumbersSeparatedByLineBreak(): void
+    public function addReturnsSumForFloatNumbersSeparatedByLineBreak(): void
     {
         $this->assertSame("3.3", $this->calculator->add("1.1\n2.2"));
     }
 
     #[Test]
-    public function itReturnsSumForFloatNumbersSeparatedByLineBreakAndCommas(): void
+    public function addReturnsSumForFloatNumbersSeparatedByLineBreakAndCommas(): void
     {
         $this->assertSame("6.6", $this->calculator->add("1.1\n2.2,3.3"));
     }
 
     #[Test]
-    public function itReturnsSumForFloatNumbersSeparatedByLineBreakAndCommasWithTrailingComma(): void
+    public function addReturnsSumForFloatNumbersSeparatedByLineBreakAndCommasWithTrailingComma(): void
     {
         $this->assertSame("6.6", $this->calculator->add("1.1\n2.2,3.3,"));
     }
 
     #[Test]
-    public function itReturnsSumForFloatNumbersThatUsesCustomSeparatorWhenProvided(): void
+    public function addReturnsSumForFloatNumbersThatUsesCustomSeparatorWhenProvided(): void
     {
         $this->assertSame("6.6", $this->calculator->add("//;\n1.1;2.2;3.3"));
     }
 
     #[Test]
-    public function itReturnsSumForFloatNumbersThatUsesLineBreakOnCustomSeparator(): void
+    public function addReturnsSumForFloatNumbersThatUsesLineBreakOnCustomSeparator(): void
     {
         $this->assertSame("6.6", $this->calculator->add("//\n\n1.1\n2.2\n3.3"));
     }
 
     #[Test]
-    public function itReturnsErrorWhenNegativeNumbersAreProvided(): void
+    public function addReturnsErrorWhenNegativeNumbersAreProvided(): void
     {
         $this->assertSame("Negative not allowed : -1\nNegative not allowed : -2", $this->calculator->add("-1,-2"));
     }
 
     #[Test]
-    public function itReturnsMultipleErrorsSeparatedByLineBreak(): void
+    public function addReturnsMultipleErrorsSeparatedByLineBreak(): void
     {
         $this->assertSame(
             "Negative not allowed : -1\nNumber expected but ',' found at position 3.",
             $this->calculator->add("-1,,2")
         );
+    }
+
+    #[Test]
+    public function multiplyReturnsZeroForEmptyString(): void
+    {
+        $this->assertSame('0', $this->calculator->multiply(''));
     }
 }
